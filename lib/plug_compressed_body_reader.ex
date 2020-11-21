e7d4746b9d5f4a8f9bcacd657bc7968121a4269f
Nico Piderman <nico.piderman@gmail.com>
2020-11-21 19:05:48 +0100

    use plug opts

--- lib/plug_compressed_body_reader.ex
+++ lib/plug_compressed_body_reader.ex
@@ -11,11 +11,15 @@ defmodule PlugCompressedBodyReader do
 
   @spec read_body(Plug.Conn.t(), Keyword.t(), Keyword.t()) ::
           {:ok, body(), Plug.Conn.t()} | {:error, String.t()}
-  def read_body(conn, _plug_opts, opts \\ []) do
-    {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
+  def read_body(conn, plug_opts, opts \\ []) do
+    with {:ok, body, conn} <- Plug.Conn.read_body(conn, plug_opts) do
+      decompress(conn, body, opts)
+    end
+  end
 
+  defp decompress(conn, body, opts) do
     with [encoding | _] <- Plug.Conn.get_req_header(conn, "content-encoding"),
-         {:ok, decompressed} <- decompress(encoding, body, opts) do
+         {:ok, decompressed} <- do_decompress(encoding, body, opts) do
       {:ok, decompressed, conn}
     else
       [] ->
@@ -26,11 +30,11 @@ defmodule PlugCompressedBodyReader do
     end
   end
 
-  defp decompress(encoding, body, opts) when encoding in ["gzip", "x-gzip"] do
+  defp do_decompress(encoding, body, opts) when encoding in ["gzip", "x-gzip"] do
     Gzip.decompress(body, opts)
   end
 
-  defp decompress(_, _, _) do
+  defp do_decompress(_, _, _) do
     {:error, "unsupported encoding"}
   end
 end
